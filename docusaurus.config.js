9559739d9498969de5a07b81546fc071d96a20c4
cuneytm <cuneyt.morten@gmail.com>
2023-05-18 10:34:17 +0000

    hadi

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,6 +54,7 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        onBrokenLinks: 'ignore', // Ignore broken links
       }),
     ],
   ],
